3e9833e499f1ee378d9931491c89d82a58f1c498
Raphael Sá <dev.raphaelsa@gmail.com>
2025-01-13 21:48:55 -0300

    Adjusted install script

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ rm -rf $TMP_DIR
 
 git clone https://github.com/odevsa/dotfiles.git $TMP_DIR
 
-# find $TMP_DIR -type f -exec sed -i "s/{user}/$USER/g" '{}' \;
+find $TMP_DIR -type f -exec sed -i "s/{user}/$USER/g" '{}' \;
 
 cp -R "$TMP_DIR/." "$HOME"
 
